1ac8c5489c213060e5e2f998d753988451c080fd
Jackson Lobo <Joacsnk@gmail.com>
2026-04-27 14:51:55 -0300

    Update settings.py
    Colocando os hosts permitidos

--- pdf_to_docx/settings.py
+++ pdf_to_docx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-68@##1(358v!3e@2^%8qo&(1m6+!&_dre6gf!2ocye8e16hjuo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
